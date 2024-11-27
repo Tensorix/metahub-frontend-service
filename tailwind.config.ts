5494e8ccfc167c385f98e9329865e7b78b989c0d
topwuther <topwuther@gmail.com>
2024-11-27 01:05:59 +0000

    fix tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/gen/**/*.ts",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     './node_modules/daisyui/dist/**/*.js',
     './node_modules/react-daisyui/dist/**/*.js',
   ],
