b74e336c37aef0705b82fa9b21db9ca2b717200e
topwuther <topwuther@gmail.com>
2024-12-07 01:28:16 +0000

    rebuild user class

--- src/componments/user.ts
+++ src/componments/user.ts
@@ -4,17 +4,24 @@ import { LoginRequest, LoginResult } from "@/gen/proto/v1/auth/login"
 import { RegisterRequest, RegisterResult } from "@/gen/proto/v1/auth/register"
 import { FriendServiceClient } from "@/gen/proto/v1/friend/friend.client"
 import { FriendListRequest, FriendListResponse } from "@/gen/proto/v1/friend/get"
+import { Message } from "@/gen/proto/v1/notify/friendmessage"
 import { Detail } from "@/gen/proto/v1/notify/heartbeat"
 import { NotifyServiceClient } from "@/gen/proto/v1/notify/notify.client"
+import { InboxProps } from "@/layout/inbox"
 import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport"
 import { Dispatch, SetStateAction } from "react"
 import { NavigateFunction } from "react-router"
 
 interface Friend {
-    tag: string
     user_id: bigint
     nickname: string
     remark: string
+    messages: Message[]
+}
+
+interface Account {
+    tag: string
+    friends: Friend[]
 }
 
 export default class User {
@@ -24,11 +31,16 @@ export default class User {
     public details?: Detail[]
     private baseUrl: string = "http://localhost:8080"
     public connected: number = 0
-    public friends: Friend[] = []
+    public inbox: InboxProps[] = []
+    public accounts: Account[] = []
+    public navigate: NavigateFunction
+    public setToast: Dispatch<SetStateAction<string>>
 
-    constructor(cookie: { [x: string]: string });
-    constructor(username: string, password: string);
-    constructor(userOrCookie: string | { [x: string]: string }, password?: string) {
+    constructor(navigate: NavigateFunction, setToast: Dispatch<SetStateAction<string>>, cookie: { [x: string]: string });
+    constructor(navigate: NavigateFunction, setToast: Dispatch<SetStateAction<string>>, username: string, password: string);
+    constructor(navigate: NavigateFunction, setToast: Dispatch<SetStateAction<string>>, userOrCookie: string | { [x: string]: string }, password?: string) {
+        this.navigate = navigate
+        this.setToast = setToast
         if (typeof userOrCookie == 'string' && password != undefined) {
             const username: string = userOrCookie
             this.username = username
@@ -78,9 +90,9 @@ export default class User {
         return response.result
     }
 
-    public AuthCheck(setToast: Dispatch<SetStateAction<string>>, navigate: NavigateFunction) {
+    public AuthCheck() {
         if (this.token == undefined) {
-            navigate("/auth/login")
+            this.navigate("/auth/login")
             return
         }
         const transport = new GrpcWebFetchTransport({
@@ -90,7 +102,7 @@ export default class User {
         const request: CheckRequest = { token: this.token }
         let response = client.check(request)
         response.status.catch((e) => {
-            setToast("network_error")
+            this.setToast("network_error")
             return
         })
         response.then(({ response }) => {
@@ -106,15 +118,13 @@ export default class User {
                     break
             }
             if (redirect) {
-                navigate("/auth/login")
+                this.navigate("/auth/login")
             }
         })
     }
-    public Heartbeat(setToast: Dispatch<SetStateAction<string>>,
-        navigate: NavigateFunction,
-        setStatus: Dispatch<SetStateAction<"error" | "success" | "warning">>) {
+    public Heartbeat(setStatus: Dispatch<SetStateAction<"error" | "success" | "warning">>) {
         if (this.token == undefined) {
-            navigate("/auth/login")
+            this.navigate("/auth/login")
             return
         }
         const transport = new GrpcWebFetchTransport({
@@ -125,24 +135,24 @@ export default class User {
         const streamcall = client.heartbeat(request)
         const limit = 1000
         let timeoutid = setTimeout(() => {
-            setToast("connection_timeout")
+            this.setToast("connection_timeout")
             setStatus("error")
         }, limit);
         streamcall.responses.onNext((message, error, complete) => {
             if (error != undefined || message == undefined || complete) {
-                setToast("network_error")
+                this.setToast("network_error")
                 setStatus("error")
                 return
             }
             switch (message.result) {
                 case CheckResult.UNSPECIFIED:
-                    setToast("unspecified_error")
+                    this.setToast("unspecified_error")
                     setStatus("error")
                     return
                 case CheckResult.SUCCESS:
                     break
                 case CheckResult.FAILED:
-                    navigate("/auth/login")
+                    this.navigate("/auth/login")
                     return
             }
             this.connected = 0
@@ -150,7 +160,7 @@ export default class User {
             clearTimeout(timeoutid)
             timeoutid = setTimeout(() => {
                 this.connected = 0
-                setToast("connection_timeout")
+                this.setToast("connection_timeout")
             }, interval + limit)
             const details = message.details
             details.forEach(detail => {
@@ -162,10 +172,9 @@ export default class User {
             this.details = details
         })
     }
-    public async GetFriendList(setToast: Dispatch<SetStateAction<string>>,
-        navigate: NavigateFunction): Promise<boolean> {
+    public async GetFriendList(): Promise<boolean> {
         if (this.token == undefined) {
-            navigate("/auth/login")
+            this.navigate("/auth/login")
             return false
         }
         const transport = new GrpcWebFetchTransport({
@@ -174,35 +183,48 @@ export default class User {
         const client = new FriendServiceClient(transport)
         const request: FriendListRequest = { token: this.token }
         let response: FriendListResponse
-        try{
+        try {
             response = await client.getFriendList(request).response
         }
-        catch(e){
-            setToast("network_error")
+        catch (e) {
+            this.setToast("network_error")
             return false
         }
         const result = response.result
         switch (result) {
             case CheckResult.UNSPECIFIED:
-                setToast("unspecified_error")
+                this.setToast("unspecified_error")
                 return false
             case CheckResult.SUCCESS:
                 break
             case CheckResult.FAILED:
-                navigate("/auth/login")
+                this.navigate("/auth/login")
                 return false
         }
         const friendList = response.friendList
-        this.friends = []
+        this.accounts = []
         friendList.forEach(friends => {
+            let account: Account = {
+                tag: friends.accountTag,
+                friends: []
+            }
             friends.friends.forEach(friend => {
-                this.friends.push({
-                    tag: friends.accountTag,
+                account.friends.push({
                     user_id: friend.userId,
                     nickname: friend.nickname,
-                    remark: friend.remark
+                    remark: friend.remark,
+                    messages: []
+                })
+                this.inbox.push({
+                    uid: friend.userId,
+                    nickname: friend.nickname,
+                    tag: account.tag,
+                    timestamp: 0,
+                    count: 0,
+                    messages: []
                 })
             })
+            this.accounts.push(account)
         })
         return true
     }
