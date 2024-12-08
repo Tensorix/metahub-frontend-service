95bf56c4709f03c45f86cd6a41a7dd68a75c09cb
topwuther <topwuther@gmail.com>
2024-12-08 05:53:58 +0000

    update inbox

--- src/layout/inbox.tsx
+++ src/layout/inbox.tsx
@@ -1,19 +1,35 @@
+import { Message, MessageType } from '@/gen/proto/v1/message/friend';
 import moment from 'moment';
 import React from 'react';
 import { Badge, Button, Indicator } from 'react-daisyui';
 
 export interface InboxProps {
+    friend_id: bigint
     nickname: string
     timestamp: number
-    message: string
+    messages: Message[]
     tag: string
     count: number
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export const Inbox: React.FC<InboxProps> = ({ nickname, timestamp, message, tag, count }) => {
+export const Inbox: React.FC<InboxProps> = ({ nickname, timestamp, messages, tag, count, onClick }) => {
+    let rawmsg = ""
+    messages.forEach(message => {
+        switch (message.type) {
+            case MessageType.UNSPECIFIED:
+            case MessageType.TEXT:
+                const decoder = new TextDecoder('utf-8')
+                rawmsg += decoder.decode(message.content)
+                break
+            case MessageType.IMAGE:
+                rawmsg += "[图片]"
+                break
+        }
+    });
     return (
         <Indicator className="w-full">
-            <Button className='h-auto' color="ghost" fullWidth>
+            <Button className='h-auto' color="ghost" fullWidth onClick={onClick}>
                 {count != 0 &&
                     <Badge color="secondary" className={Indicator.Item.className()}>
                         {count}
@@ -22,10 +38,10 @@ export const Inbox: React.FC<InboxProps> = ({ nickname, timestamp, message, tag,
                 <div className="w-full max-w-xs justify-center">
                     <label className="label cursor-pointer">
                         <span className="label-text">{nickname}</span>
-                        <span className="label-text-alt">{moment(timestamp).format("YYYY-MM-DD")}</span>
+                        <span className="label-text-alt">{timestamp != 0 && moment(timestamp * 1000).format("YYYY-MM-DD")}</span>
                     </label>
                     <label className="label cursor-pointer">
-                        <span className="label-text">{message}</span>
+                        <span className="label-text">{rawmsg}</span>
                         <span className="label-text-alt">{tag}</span>
                     </label>
                 </div>
